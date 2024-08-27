ad158c0bb6f53c3ffc9b55db2cabb52ef5b970f1
unknown <mn1520@nyu.edu>
2024-08-27 11:14:33 -0400

    refacted more code to dbhandler and reqresulthandler plese test ty

--- classes/DatabaseHandler.py
+++ classes/DatabaseHandler.py
@@ -2,21 +2,58 @@ class DatabaseHandler:
     def __init__(self, db_session):
         self.db_session = db_session
 
-    def store_data(self, data):
-        # Store the data in the database
-        for item in data:
-            shop = Shops(
-                owner=item['owner'],
-                title=item['title'],
-                map_location=item['map_location'],
-                map_x=item['map_x'],
-                map_y=item['map_y'],
-                item_id=item['item_id'],
-                price=item['price'],
-                timestamp=item['timestamp']
+    # def store_data(self, data):
+    #     # Store the data in the database
+    #     for item in data:
+    #         shop = Shops(
+    #             owner=item['owner'],
+    #             title=item['title'],
+    #             map_location=item['map_location'],
+    #             map_x=item['map_x'],
+    #             map_y=item['map_y'],
+    #             item_id=item['item_id'],
+    #             price=item['price'],
+    #             timestamp=item['timestamp']
+    #         )
+    #         self.db_session.add(shop)
+    #     self.db_session.commit()
+
+
+    def upsert_shop(self, shop_data, generation_timestamp):
+        # Check if the shop already exists based on owner and timestamp
+        existing_shop = self.db_session.query(Shops).filter_by(
+            owner=shop_data['owner'], 
+            timestamp=shop_data['timestamp']
+        ).first()
+
+        if existing_shop:
+            # Update existing shop req_timestamp and get the ShopID
+            existing_shop.req_timestamp = generation_timestamp
+
+            #query all shops_item using ShopID
+            #for queried_item in existing_shop:
+                #compare to counterpart in shop_data["items"]
+                    #if no match, delete the row
+                    #if match, update the row
+
+        else:
+            # Create a new shop record
+            new_shop = Shops(
+                owner=shop_data['owner'],
+                title=shop_data['title'],
+                map_location=shop_data['map_location'],
+                map_x=shop_data['map_x'],
+                map_y=shop_data['map_y'],
+                item_id=shop_data['item_id'],
+                price=shop_data['price'],
+                timestamp=shop_data['timestamp']
             )
-            self.db_session.add(shop)
+            self.db_session.add(new_shop)
+
         self.db_session.commit()
+
+
+
 
     def update_latest_timestamp(self, timestamp):
         # Update the metadata table with the latest timestamp
--- classes/ReqResultHandler.py
+++ classes/ReqResultHandler.py
@@ -1,9 +1,22 @@
-
+from models import Shops
 
 class ReqResultHandler:
-    def __init__(self, db_session):
+    def __init__(self, db_session, db_handler, api_handler):
         self.db_session = db_session
+        self.db_handler = db_handler
+        self.api_handler = api_handler
 
     def filter_shops(self):
-        pass
+        results = self.api_handler.request_shop_data()
 
+        items_ids = #get all item IDs
+
+        shops_res = [shop for shop in results["shops"] if shop["type"]=="V"]
+        #get the selling shops from the response
+
+        generation_timestamp = results["generation_timestamp"]
+        #get the generation timestamp from results for Shop req_timestamp col
+
+        # Upsert the shop data
+        for shop_data in shops_res:
+            self.db_handler.upsert_shop(shop_data, generation_timestamp)
